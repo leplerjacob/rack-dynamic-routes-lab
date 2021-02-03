0a3d65011108f7be2b1b9b0fcd6c9f93db90cb9c
Jacob Lepler <lepler.jacob@gmail.com>
2021-02-03 08:21:36 -0500

    commit tree 8d0e9e9559fe92618e14a908a7b5392bd172aeb2

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,15 @@
 class Item
   attr_accessor :name, :price
 
+  @@item = []
+
   def initialize(name,price)
+    @@item << self
     @name = name
     @price = price
+  end
+
+  def self.all
+    @@item
   end
 end
